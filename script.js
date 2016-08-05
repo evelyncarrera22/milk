bcf881632f93b4d2dc0b9ee34dc18ac482d0ecf1
ScriptEd <scripted@ScriptEd-C02H6352DV7N.local>
2016-08-05 11:05:59 -0400

    fixed jquery and made it function

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-$( "#More" ).click(function() {
- for ( var i=0; i<100; i++){
-  $( "body" ).append('<img src="http://i.imgur.com/GPRGZBB.png"></img>');
-}
-});
+$( document ).ready(function() {
 
-$("#milk").click(function()  {
-  for ( var i=100; i>1; i--){
-    $("img").remove();
-  }
+	$( "#more" ).click(function() {
+	 for ( var i=0; i<100; i++){
+	  $( "body" ).append('<img src="http://i.imgur.com/GPRGZBB.png"></img>');
+	}
+	});
+
+	$("#milk").click(function()  {
+	  for ( var i=100; i>1; i--){
+	    $("img").remove();
+	  }
+	});
 });
